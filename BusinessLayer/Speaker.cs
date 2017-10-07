3dd04fa08900fcf04761f9d0ce4f1c8fba09a2c6
VeikoQuiroz <veiko.quiroz@gmail.com>
2017-10-07 11:37:21 -0500

    Initial commit

---
+++ BusinessLayer/Speaker.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BusinessLayer
+{
+    /// <summary>
+    /// Represents a single speaker
+    /// </summary>
+    public class Speaker
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+		public int? yearExperience { get; set; }
+		public bool HasBlog { get; set; }
+		public string BlogURL { get; set; }
+		public WebBrowser Browser { get; set; }
+		public List<string> Certifications { get; set; }
+		public string Employer { get; set; }
+		public int RegistrationFee { get; set; }
+		public List<BusinessLayer.Session> Sessions { get; set; }
+
+		/// <summary>
+		/// Register a speaker
+		/// </summary>
+		/// <returns>speakerID</returns>
+		public int? Register(IRepository repository)
+		{
+            validarRegistro();
+            int registrationFee = repository.ObtenerRegistrationFee(yearExperience);
+            int speakerID = repository.SaveSpeaker(this);
+            return speakerID;
+		}
+
+        private void validarRegistro()
+        {
+            validarCamposLlenos();
+            if (!cumpleRequisitos())
+            {
+                throw new SpeakerDoesntMeetRequirementsException("Speaker doesn't meet our abitrary and capricious standards.");
+            }
+            Aprobado();
+        }
+
+        private void validarCamposLlenos()
+        {
+            if (string.IsNullOrEmpty(FirstName)) throw new ArgumentNullException("First Name is required.");
+            if (string.IsNullOrEmpty(LastName)) throw new ArgumentNullException("Last Name is required.");
+            if (string.IsNullOrEmpty(Email)) throw new ArgumentNullException("Email is required.");
+            if (Sessions.Count() == 0) throw new ArgumentException("Can't register speaker with no sessions to present.");
+        }
+
+        private bool cumpleRequisitos()
+        {
+            return esExperto() || !validaMailyNavegador();
+        }
+        
+        private bool esExperto()
+        {
+            if (yearExperience > 10) return true;
+            if (HasBlog) return true;            
+            if (Certifications.Count() > 3) return true;
+            if (buenEmpleo()) return true;
+            return false;
+        }
+
+        private bool buenEmpleo()
+        {
+            string[] empresas = new string[] { "Microsoft", "Google", "Fog Creek Software", "37Signals" };
+            return empresas.Contains(Employer);
+        }
+
+        private bool validaMailyNavegador()
+        {
+            return antiguoDominio() || antiguoNavegador();
+        }
+
+        private bool antiguoDominio()
+        {
+            string[] listaEmailDomain = new string[] { "aol.com", "hotmail.com", "prodigy.com", "CompuServe.com" };
+            string emailDomain = Email.Split('@').Last();
+            return listaEmailDomain.Contains(emailDomain);
+        }
+
+        private bool antiguoNavegador()
+        {
+            return Browser.Name == WebBrowser.BrowserName.InternetExplorer && Browser.MajorVersion < 9;
+        }
+
+        private void Aprobado()
+        {
+            foreach (var session in Sessions)
+            {
+                session.Approved = !EsAntiguaTecnologia(session.Description);
+            }
+
+            bool noSessionsApproved = Sessions.Where(s => s.Approved).Count() == 0;
+            if (noSessionsApproved) throw new NoSessionsApprovedException("No sessions approved.");
+        }
+                       
+        private bool EsAntiguaTecnologia(string sessionDescription)
+        {
+            string[] oldTechnologies = new string[] { "Cobol", "Punch Cards", "Commodore", "VBScript" };
+            foreach (var oldTech in oldTechnologies)
+            {
+                if (sessionDescription.Contains(oldTech)) return true;
+            }
+            return false;
+        }
+
+        #region Custom Exceptions
+        public class SpeakerDoesntMeetRequirementsException : Exception
+		{
+			public SpeakerDoesntMeetRequirementsException(string message)
+				: base(message)
+			{
+			}
+
+			public SpeakerDoesntMeetRequirementsException(string format, params object[] args)
+				: base(string.Format(format, args)) { }
+		}
+
+		public class NoSessionsApprovedException : Exception
+		{
+			public NoSessionsApprovedException(string message)
+				: base(message)
+			{
+			}
+		}
+		#endregion
+	}
+}
---
+++ DataAccessLayer/SqlServerCompactRepository.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer;
+
+namespace DataAccessLayer
+{
+    public class SqlServerCompactRepository : IRepository
+	{
+		public int SaveSpeaker(Speaker speaker)
+		{
+			//TODO: Save speaker to DB for now. For demo, just assume success and return 1.
+			return 1;
+		}
+        public int ObtenerRegistrationFee(int? yearExperience)
+        {
+            /*
+             Se trabajaría con una tabla que tenga 3 campos
+             id  MaxExperiencia   RegistrationFee
+             1     1                500
+             2     3                250
+             3     5                100
+             4     9                50
+            
+             */
+            return 50;
+        }
+        
+    }
+}
